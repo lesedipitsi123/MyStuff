e2316d552e3c09995763504e26f61f70146ce9b2
lesedipitsi123 <lesedipitsi@gmail.com>
2019-02-21 17:27:36 +0200

    Ran tests

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,18 +1,27 @@
 using NUnit.Framework;
+using Problems;
 
 namespace Tests
 {
     public class Tests
     {
+        private  Program _problems;
+
         [SetUp]
         public void Setup()
         {
+        _problems = new Program();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var test1= _problems.Prefix("hello"); 
+            Assert.That(test1, Is.EqualTo("5,1:hello"));
+            var test2 = _problems.Prefix("");
+            Assert.That(test2, Is.EqualTo("0,0:"));
+            var test3 = _problems.Prefix("what ... did you say??");
+            Assert.That(test3, Is.EqualTo("22,5:what ... did you say??"));
         }
     }
 }
