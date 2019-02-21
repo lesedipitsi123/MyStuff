da96e264eb6aa1715b8792def4dbd337de55249a
lesedipitsi123 <lesedipitsi@gmail.com>
2019-02-21 16:14:25 +0200

    Added Problems.Tests

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Text;
 
 namespace Problems
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
+        }
+
+        // Prefixes string
+        static string Prefix(string  input)
+        {
+            if(input == null) return null;
+            StringBuilder sb= new StringBuilder();
+            int len = input.Length;
+        
+            sb.Append(len).Append(',');
+            // Append number of words in string
+            int words=0;
+            for(int i=0; i< len; i++)
+            {
+                
+                    if(String.IsNullOrWhiteSpace(input[i].ToString())){
+                    words+=1;  
+                    }
+            }
+
+            sb.Append(words).Append(':').Append(input);
+
+            return sb.ToString();
         }
     }
 }
