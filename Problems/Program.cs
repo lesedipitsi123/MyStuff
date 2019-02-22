116ba74bdd2d47773b2464d592e2f19853e27d74
lesedipitsi123 <lesedipitsi@gmail.com>
2019-02-22 11:10:12 +0200

    Made Prefix accessible

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -12,19 +12,23 @@ namespace Problems
         }
 
         // Prefixes string
-        static string Prefix(string  input)
+       public string Prefix(string  input)
         {
             if(input == null) return null;
             StringBuilder sb= new StringBuilder();
             int len = input.Length;
-        
+            if(len < 1)
+            {
+                return "0,0:";
+            }
             sb.Append(len).Append(',');
             // Append number of words in string
-            int words=0;
-            for(int i=0; i< len; i++)
+            int words=1; // len >= 1 so at least 1 words
+            for(int i=0; i < len; i++)
             {
                 
-                    if(String.IsNullOrWhiteSpace(input[i].ToString())){
+                    if(String.IsNullOrWhiteSpace(input[i].ToString()))
+                    {
                     words+=1;  
                     }
             }
